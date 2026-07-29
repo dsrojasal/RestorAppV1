ee6dae7c85e6f11234b712b85f94c9a58f98c906
Daniel Rojas <danielsantiagorojas123@gmail.com>
2026-07-29 15:57:45 -0500

    feat: add @Public() decorator and update JwtAuthGuard for global auth

--- src/custom.decorator.ts
+++ src/custom.decorator.ts
@@ -1,5 +1,8 @@
 import { SetMetadata } from '@nestjs/common';
-import { Role } from './users/enums/role.enum';
+import { Role } from './common/enums/role.enum';
 
 export const ROLES_KEY = 'roles';
 export const Roles = (...roles: Role[]) => SetMetadata(ROLES_KEY, roles);
+
+export const IS_PUBLIC_KEY = 'isPublic';
+export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
