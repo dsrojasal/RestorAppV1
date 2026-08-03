83ad01efa6e18efc0e5032f0fb984ef98f6f4c24
Daniel Rojas <danielsantiagorojas123@gmail.com>
2026-08-03 13:29:29 -0500

    feat(usuarios): permitir actualizar la contraseña sin error de validación

--- src/usuarios/dto/update-usuario.dto.ts
+++ src/usuarios/dto/update-usuario.dto.ts
@@ -1,4 +1,11 @@
 import { PartialType, OmitType } from '@nestjs/mapped-types';
+import { IsOptional, IsString, MinLength, MaxLength } from 'class-validator';
 import { CreateUsuarioDto } from './create-usuario.dto';
 
-export class UpdateUsuarioDto extends PartialType(OmitType(CreateUsuarioDto, ['password'] as const)) {}
+export class UpdateUsuarioDto extends PartialType(OmitType(CreateUsuarioDto, ['password'] as const)) {
+  @IsOptional()
+  @IsString()
+  @MinLength(8)
+  @MaxLength(72)
+  password?: string;
+}
